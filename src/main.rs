e06ae2f12bf3461e4a13d590f3b94529f995131a
soli <crystalficus@gmail.com>
2025-01-06 12:39:25 -0600

    add vertex buffer, following wgpu basic tutorial

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use glyphon::{
 };
 use std::sync::{Arc, Once};
 use wgpu::{
+    util::DeviceExt,
     CommandEncoderDescriptor, CompositeAlphaMode, DeviceDescriptor, Instance, InstanceDescriptor,
     LoadOp, MultisampleState, Operations, PresentMode, RenderPassColorAttachment,
     RenderPassDescriptor, RequestAdapterOptions, SurfaceConfiguration, TextureFormat,
@@ -25,6 +26,22 @@ fn main() {
         .unwrap();
 }
 
+
+// unsafe impl bytemuck::Pod for Vertex {}
+// unsafe impl bytemuck::Zeroable for Vertex {}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+];
+
 struct WindowState {
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -38,8 +55,24 @@ struct WindowState {
     text_buffer: glyphon::Buffer,
     chat_text: String,
     render_pipeline: wgpu::RenderPipeline,
-
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
     window: Arc<Window>,
+}
+
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }        
+    }
 }
 
 impl WindowState {
@@ -55,6 +88,14 @@ impl WindowState {
             .request_device(&DeviceDescriptor::default(), None).await.unwrap();
         let surface = instance.create_surface(window.clone()).expect("Create Surface");
 
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+        let num_vertices = VERTICES.len() as u32;
         //Render Pipeline
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
@@ -74,7 +115,9 @@ impl WindowState {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
-                buffers: &[],
+                buffers: &[
+                    Vertex::desc(),
+                ],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             },
             fragment: Some(wgpu::FragmentState {
@@ -154,7 +197,8 @@ impl WindowState {
             text_buffer,
             chat_text,
             render_pipeline,
-
+            vertex_buffer,
+            num_vertices,
             window,
             }
         }
@@ -199,9 +243,13 @@ impl WindowState {
                 text_buffer,
                 chat_text,
                 render_pipeline,
+                vertex_buffer,
+                num_vertices,
                 ..
             } = state;
             let chat_text = &mut state.chat_text;
+            //Will be used for command mapping
+            let mut key_table = vec![false; 255].into_boxed_slice();
 
             match event {
                 WindowEvent::KeyboardInput {
@@ -213,6 +261,10 @@ impl WindowState {
                         },
                     ..
                 } => {
+                    // if let PhysicalKey::Code(code) = event.physical_key {
+                    //     key_table[code as usize] = event.state.is_pressed();
+                    // }
+
                     let inputMode = true;
                     info!("hi hi hi");
                     console::write_to_console(text_buffer, font_system, chat_text, "hi");
@@ -255,6 +307,8 @@ impl WindowState {
                     let frame = surface.get_current_texture().unwrap();
                     let view = frame.texture.create_view(&TextureViewDescriptor::default());
                     let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor { label: None });
+
+
                     {
                         let mut pass = encoder.begin_render_pass(&RenderPassDescriptor {
                             label: Some("Render pass"),
@@ -272,7 +326,8 @@ impl WindowState {
                     });
                     text_renderer.render(&atlas, &viewport, &mut pass).unwrap();
                     pass.set_pipeline(&render_pipeline);
-                    pass.draw(0..3,0..1);
+                    pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                    pass.draw(0..*num_vertices, 0..1);                    
                 }
                 queue.submit(Some(encoder.finish()));
                 frame.present();
