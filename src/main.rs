65cd087f8e862388e5dae046d1946d36c8bd616d
soli <crystalficus@gmail.com>
2025-01-13 17:54:38 -0600

    Reimplement indices since it was rolled back

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,22 @@ struct Vertex {
     color: [f32; 3],
 }
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 struct WindowState {
@@ -56,7 +69,8 @@ struct WindowState {
     chat_text: String,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
     window: Arc<Window>,
     diffuse_bind_group: wgpu::BindGroup,
 }
@@ -98,6 +112,14 @@ impl WindowState {
                 usage: wgpu::BufferUsages::VERTEX,
             }
         );
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+        let num_indices = INDICES.len() as u32;
         let num_vertices = VERTICES.len() as u32;
         //Render Pipeline
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
@@ -107,7 +129,7 @@ impl WindowState {
         let render_pipeline_layout =
         device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[&texture_bind_group_layout],
+            bind_group_layouts: &[],            
             push_constant_ranges: &[],
         });
         let swapchain_format = TextureFormat::Bgra8UnormSrgb;
@@ -297,7 +319,8 @@ impl WindowState {
             chat_text,
             render_pipeline,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
             window,
             diffuse_bind_group,
             }
@@ -344,7 +367,8 @@ impl WindowState {
                 chat_text,
                 render_pipeline,
                 vertex_buffer,
-                num_vertices,
+                index_buffer,
+                num_indices,
                 diffuse_bind_group,
                 ..
             } = state;
@@ -427,9 +451,10 @@ impl WindowState {
                     });
                     text_renderer.render(&atlas, &viewport, &mut pass).unwrap();
                     pass.set_pipeline(&render_pipeline);
-                    pass.set_bind_group(0, diffuse_bind_group, &[]);
+                    pass.set_bind_group(0, &*diffuse_bind_group, &[]);
                     pass.set_vertex_buffer(0, vertex_buffer.slice(..));
-                    pass.draw(0..*num_vertices, 0..1);                    
+                    pass.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint16);  
+                    pass.draw(0..*num_indices, 0..1);                    
                 }
 
                 queue.submit(Some(encoder.finish()));
